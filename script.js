3447391715dbdf612c572ffd6131767fb6ecd3d5
Taha <taha-chaoui@hotmail.com>
2022-01-04 11:31:41 +0100

    doing well

--- script.js
+++ script.js
@@ -3,29 +3,48 @@ function computerPlay() {
   return arrGame[Math.floor(Math.random() * arrGame.length)];
 }
 
-console.log(computerPlay());
+// alert(computerPlay());
 
-function playRound(playerSelection, computerSelection) {
-  if (playerSelection === "rock" && computerSelection === "paper") {
-    ("You Lose! Paper beats Rock");
-  } else if (playerSelection === "paper" && computerSelection === "scissors") {
-    return "You Lose! Scissors beats Paper";
-  } else if (playerSelection === "scissors" && computerSelection === "rock") {
-    return "You Lose! Rock beats Scissors";
-  } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    return "You Win! Scissors beats Paper";
-  } else if (playerSelection === "paper" && computerSelection === "rock") {
-    return "You Win! Paper beats Rock";
-  } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    return "You Win! Rock beats Scissors";
-  } else if (playerSelection === computerSelection) {
-    return "Equal!!!";
-  } else {
-    return "ERROR";
+function game(playRound) {
+  let i = 0;
+  while (i < 5) {
+    function playRound(playerSelection, computerSelection) {
+      if (playerSelection === "rock" && computerSelection === "paper") {
+        ("You Lose! Paper beats Rock");
+      } else if (
+        playerSelection === "paper" &&
+        computerSelection === "scissors"
+      ) {
+        return "You Lose! Scissors beats Paper";
+      } else if (
+        playerSelection === "scissors" &&
+        computerSelection === "rock"
+      ) {
+        return "You Lose! Rock beats Scissors";
+      } else if (
+        playerSelection === "scissors" &&
+        computerSelection === "paper"
+      ) {
+        return "You Win! Scissors beats Paper";
+      } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You Win! Paper beats Rock";
+      } else if (
+        playerSelection === "rock" &&
+        computerSelection === "scissors"
+      ) {
+        return "You Win! Rock beats Scissors";
+      } else if (playerSelection === computerSelection) {
+        return "Equal!!!";
+      } else {
+        return "ERROR";
+      }
+    }
+    const playerSelection = prompt().toLowerCase();
+    const computerSelection = computerPlay();
+    alert(playRound(playerSelection, computerSelection));
+    i++;
   }
+  return playRound();
 }
 
-const playerSelection = prompt().toLowerCase();
-const computerSelection = computerPlay();
-
-console.log(playRound(playerSelection, computerSelection));
+alert(game());
