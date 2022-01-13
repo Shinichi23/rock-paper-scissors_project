c9d4cc91174e8ecae679ce6777d88d860be7e97e
Taha <taha-chaoui@hotmail.com>
2022-01-13 17:35:17 +0100

    90% time to style

--- script.js
+++ script.js
@@ -19,38 +19,44 @@ function playRound(playerSelection, computerSelection) {
   } else if (playerSelection === "rock" && computerSelection === "paper") {
     return `You Lose! Paper beats Rock ${(document.getElementById(
       "computerScore"
-    ).textContent = `Computer : ${(computerScore += 1)}`)}`;
+    ).textContent = `Computer score : ${(computerScore += 1)}`)}`;
   } else if (playerSelection === "paper" && computerSelection === "scissors") {
     return `You Lose! Scissors beats Paper ${(document.getElementById(
       "computerScore"
-    ).textContent = `Computer : ${(computerScore += 1)}`)}`;
+    ).textContent = `Computer score : ${(computerScore += 1)}`)}`;
   } else if (playerSelection === "scissors" && computerSelection === "rock") {
     return `You Lose! Rock beats Scissors ${(document.getElementById(
       "computerScore"
-    ).textContent = `Computer : ${(computerScore += 1)}`)}`;
+    ).textContent = `Computer score : ${(computerScore += 1)}`)}`;
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
     return `You Win! Scissors beats Paper ${(document.getElementById(
       "playerScore"
-    ).textContent = `Player : ${(playerScore += 1)}`)}`;
+    ).textContent = `Player score : ${(playerScore += 1)}`)}`;
   } else if (playerSelection === "paper" && computerSelection === "rock") {
     return `You Win! Paper beats Rock ${(document.getElementById(
       "playerScore"
-    ).textContent = `Player : ${(playerScore += 1)}`)}`;
+    ).textContent = `Player score : ${(playerScore += 1)}`)}`;
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
     return `You Win! Rock beats Scissors ${(document.getElementById(
       "playerScore"
-    ).textContent = `Player : ${(playerScore += 1)}`)}`;
+    ).textContent = `Player score : ${(playerScore += 1)}`)}`;
   }
 }
 
 function gameScore(playerScore, computerScore) {
   if (playerScore == 5 && computerScore < playerScore) {
-    alert("You Win!");
+    alert("You Win!"),
+      document.getElementById("rock").setAttribute("disabled", 1),
+      document.getElementById("paper").setAttribute("disabled", 1),
+      document.getElementById("scissors").setAttribute("disabled", 1),
+      document.getElementById("resetGame").removeAttribute("disabled");
   } else if (computerScore == 5 && computerScore > playerScore)
-    alert("You Lose!");
+    alert("You Lose!"),
+      document.getElementById("rock").setAttribute("disabled", 1),
+      document.getElementById("paper").setAttribute("disabled", 1),
+      document.getElementById("scissors").setAttribute("disabled", 1),
+      document.getElementById("resetGame").removeAttribute("disabled");
 }
-
-const resetGame = document.querySelector("#resetGame");
 
 function game(player) {
   player.forEach((play) => {
@@ -63,3 +69,21 @@ function game(player) {
 }
 
 game(player);
+
+function resetGame() {
+  return (
+    document.getElementById("rock").removeAttribute("disabled"),
+    document.getElementById("paper").removeAttribute("disabled"),
+    document.getElementById("scissors").removeAttribute("disabled"),
+    (document.getElementById(
+      "playerScore"
+    ).textContent = `Player Score : ${(playerScore = 0)}`),
+    (document.getElementById(
+      "computerScore"
+    ).textContent = `Computer Score : ${(computerScore = 0)}`)
+  );
+}
+
+document.getElementById("resetGame").addEventListener("click", function (e) {
+  resetGame(e);
+});
